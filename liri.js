9e1e63957873934a770073c04f221e2771984b4d
glaracuente <glaracuente@outbrain.com>
2018-11-06 14:00:37 -0500

    updates

--- liri.js
+++ liri.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
-var keys = require("key.js");
+var keys = require("./keys.js");
+var request = require("request");
+
 
 //concert-this
+artist = "weeknd";
+
+request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body));
+  }
+});
 
 //spotify-this-song
 

2f2fbbdb33f63c655b5e2976fafed19e407fce02
glaracuente <glaracuente@outbrain.com>
2018-11-06 15:00:28 -0500

    updates

--- liri.js
+++ liri.js
@@ -2,18 +2,45 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 
-
+/*
 //concert-this
 artist = "weeknd";
 
-request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log(JSON.parse(body));
-  }
+request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(JSON.parse(body));
+    }
 });
 
-//spotify-this-song
 
 //movie-this
+movie = "venom";
+
+request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(JSON.parse(body));
+    }
+});
+
+
+
+
+*/
+
+//spotify-this-song
+song = "madura"
+var SpotifyAPI = require('node-spotify-api');
+var spotify = new SpotifyAPI(keys.spotify);
+
+spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+  console.log(data.tracks.items);
+  });
+
+
+
 
 //do-what-it-says

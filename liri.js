ceb6596d0337e7c1a112990690b2582e0bf78a86
glaracuente <glaracuente@outbrain.com>
2018-11-06 19:33:42 -0500

    updates

--- liri.js
+++ liri.js
@@ -2,45 +2,42 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 
-/*
-//concert-this
-artist = "weeknd";
-
-request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.parse(body));
-    }
-});
+var userRequest = process.argv[2]
+var userFilter = process.argv[3]
 
 
-//movie-this
-movie = "venom";
+if (userRequest === "concert-this") {
+    request("https://rest.bandsintown.com/artists/" + userFilter + "/events?app_id=codingbootcamp", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body));
+        }
+    });
+}
 
-request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.parse(body));
-    }
-});
+if (userRequest === "spotify-this-song") {
+    var SpotifyAPI = require('node-spotify-api');
+    var spotify = new SpotifyAPI(keys.spotify);
+
+    spotify.search({ type: 'track', query: userFilter, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.tracks.items);
+    });
+
+}
+
+if (userRequest === "movie-this") {
+    request("http://www.omdbapi.com/?apikey=trilogy&t=" + userFilter, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body));
+        }
+    });
+}
+
+if (userRequest === "do-what-it-says") {
+
+}
 
 
-
-
-*/
-
-//spotify-this-song
-song = "madura"
-var SpotifyAPI = require('node-spotify-api');
-var spotify = new SpotifyAPI(keys.spotify);
-
-spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-
-  console.log(data.tracks.items);
-  });
-
-
-
-
-//do-what-it-says

c762366cf15ce388ea2c2199b15047e26aa2b44e
glaracuente <glaracuente@outbrain.com>
2018-11-08 03:23:29 -0500

    updates

--- liri.js
+++ liri.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 var moment = require('moment');
+var fs = require("fs");
+
 
 var userRequest = process.argv[2]
 var userFilter = process.argv[3]
-var usage = "\nUsage: node liri.js OPTION FILTER\n\nValid OPTIONs are:\nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says"
+var usage = "\nUsage: node liri.js OPTION FILTER\n\nValid OPTIONs are:\nconcert-this '<artist/band name here>'\nExample: concert-this 'ozuna'\n\nspotify-this-song '<song title here>'\nExample: spotify-this-song 'thriller'\n\nmovie-this '<movie name here>'\nExample: movie-this 'harry potter'\n\ndo-what-it-says\nExample: do-what-it-says"
 var validOptions = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
 
 if (validOptions.indexOf(userRequest) === -1) {
@@ -13,60 +15,132 @@ if (validOptions.indexOf(userRequest) === -1) {
     return;
 }
 
+
 if (userRequest === "concert-this") {
-    request("https://rest.bandsintown.com/artists/" + userFilter + "/events?app_id=codingbootcamp", function (error, response, body) {
+    concertThis(userFilter)
+}
+
+function concertThis(filter) {
+    request("https://rest.bandsintown.com/artists/" + filter + "/events?app_id=codingbootcamp", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var data = JSON.parse(body);
 
             for (var i = 0; i < data.length; i++) {
-                console.log("----- Venue #" + i + " -----");
-                console.log("Name: " + data[0].venue.name);
-                console.log("Location: " + data[0].venue.city + ", " + data[0].venue.country);
-                console.log("Date: " + moment(data[0].datetime).format('MM/DD/YYYY'));
+                var output = "----- Venue #" + i + " -----" + "\n" +
+                    "Name: " + data[i].venue.name + "\n" +
+                    "Lineup: " + data[i].lineup + "\n" +
+                    "Location: " + data[i].venue.city + ", " + data[0].venue.country + "\n" +
+                    "Date: " + moment(data[i].datetime).format('MM/DD/YYYY') + "\n";
+
+                console.log(output)
+                logIt(output)
             }
         }
     });
 }
 
+
 if (userRequest === "spotify-this-song") {
+    spotifyThis(userFilter)
+}
+
+function spotifyThis(filter) {
     var SpotifyAPI = require('node-spotify-api');
     var spotify = new SpotifyAPI(keys.spotify);
 
-    spotify.search({ type: 'track', query: userFilter, limit: 1 }, function (err, data) {
+    var song = filter;
+
+    if (filter === undefined || filter.length === 0) {
+        song = "The Sign"
+    }
+
+    spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("Artists:")
-        for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
-            console.log(data.tracks.items[0].artists[i].name);
+
+        for (var a = 0; a < data.tracks.items.length; a++) {
+            var output = "----- Song #" + eval(a + 1) + " -----" + "\n";
+            var artists = [];
+            for (var b = 0; b < data.tracks.items[a].artists.length; b++) {
+                artists.push(data.tracks.items[a].artists[b].name);
+            }
+            output = output + "Artists: " + artists + "\n" +
+                "Song Name: " + data.tracks.items[a].name + "\n" +
+                "Preview URL: " + data.tracks.items[a].preview_url + "\n" +
+                "Album: " + data.tracks.items[a].album.name + "\n";
+
+            console.log(output)
+            logIt(output)
         }
-        console.log("Song Name:")
-        console.log(data.tracks.items[0].name);
-        console.log("Preview URL:")
-        console.log(data.tracks.items[0].preview_url);
-        console.log("Album:")
-        console.log(data.tracks.items[0]);
+
+
 
     });
 
 }
 
 if (userRequest === "movie-this") {
-    request("http://www.omdbapi.com/?apikey=trilogy&t=" + userFilter, function (error, response, body) {
+    movieThis(userFilter)
+}
+
+function movieThis(filter) {
+    var movie = filter;
+
+    if (filter === undefined || filter.length === 0) {
+        movie = "Mr. Nobody"
+    }
+
+    request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log(JSON.parse(body));
+            data = JSON.parse(body);
+
+            var output = "Title: " + data.Title + "\n" +
+                "Year: " + data.Year + "\n" +
+                "imdbRating: " + data.imdbRating + "\n" +
+                "Country: " + data.Country + "\n";
+            for (var i = 0; i < data.Ratings.length; i++) {
+                if (data.Ratings[i].Source === "Rotten Tomatoes") {
+                    output = output + "Rotten Tomatoes Rating: " + data.Ratings[i].Value + "\n";
+                }
+
+            }
+            output = output + "Language: " + data.Language + "\n" +
+                "Plot: " + data.Plot + "\n" +
+                "Actors: " + data.Actors + "\n";
+
+            console.log(output)
+            logIt(output)
         }
     });
-   /* * Title of the movie.
-  * Year the movie came out.
-  * IMDB Rating of the movie.
-  * Rotten Tomatoes Rating of the movie.
-  * Country where the movie was produced.
-  * Language of the movie.
-  * Plot of the movie.
-  * Actors in the movie.*/
+
 }
 
 if (userRequest === "do-what-it-says") {
-  fs.readFIle("random.txt")
+    var requestFromFile = "";
+    var filterFromFile = "";
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        requestFromFile = dataArr[0];
+        requestAsFunctionName = requestFromFile.split("-")[0] + "This"
+        filterFromFile = dataArr[1];
+        eval(requestAsFunctionName + "(" + filterFromFile + ")")
+    });
 }
+
+
+
+function logIt(textBody) {
+    fs.appendFile("log.txt", textBody + "\n\n", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
